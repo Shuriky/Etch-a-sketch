07cbe5857567bf1fcb50e92c904ba7ca8b7a24b2
Shuriky <hdoo0027@student.monash.edu>
2025-04-19 02:16:44 +1000

    add the hover effect

--- script.js
+++ script.js
@@ -1,30 +1,68 @@
 console.log("hello world")
+document.title = "Etch-A-Sketch"
 
 const noRows = 16;
 const noColumns = 16;
 
+// Sketch on-off check
+let sketchStatus = false;
+const sketchButton = document.createElement("input");
+sketchButton.type = 'checkbox';
+const sketchLabel = document.createElement("label");
+sketchLabel.textContent = 'Grid on/off'
 
-let container = document.createElement("div");
-container.style.display = "flex"; // This makes it a flex container
-container.style.justifyContent = 'center';
-container.style.alignItems = 'center';
+// Add event listener to the checkbox to detect changes 
+sketchButton.addEventListener('change', function() {
+    // Add the hover effect
+    let tiles = document.querySelectorAll('.tile');
+    tiles.forEach(til => {
+        if(sketchButton.checked) {
+            til.addEventListener('mouseenter', mouseEnter);
+            til.addEventListener('mouseleave', mouseLeave);
+            }
+        else {
+            til.removeEventListener('mouseenter', mouseEnter);
+            til.removeEventListener('mouseleave', mouseLeave);          
+        }
+    });
 
+});
 
-for (let i = 0; i < noColumns; i++) {
-    let row = document.createElement("row");
-    row.style.flexWrap = "wrap";
+document.body.appendChild(sketchButton);
+document.body.appendChild(sketchLabel);
 
-    for (let j = 0; j < noRows; j++) {
-        let tile = document.createElement("div");
-        tile.className = "tile";
-        tile.style.backgroundColor = 'White';
-        tile.style.width = '50px';
-        tile.style.height = '50px';
-         tile.style.border = "1px solid black";
-        tile.style.margin = '5px';
+// Create the grid
+function createGrid() {
+    let container = document.createElement("div");
+    container.style.display = "flex"; // This makes it a flex container
+    container.style.justifyContent = 'center';
+    container.style.alignItems = 'center';
 
-        row.appendChild(tile);
+    for (let i = 0; i < noColumns; i++) {
+        let row = document.createElement("div");
+        row.style.flexWrap = "wrap";
+
+        for (let j = 0; j < noRows; j++) {
+            let tile = document.createElement("div");
+            tile.className = "tile";
+            tile.style.backgroundColor = 'White'; 
+            tile.style.width = '30px';  
+            tile.style.height = '30px'; 
+            tile.style.border = "1px solid black";
+            tile.style.margin = '3px'; // Change spacing of tiles
+            row.appendChild(tile);
+        }
+
+        container.appendChild(row);
     }
-    container.appendChild(row);
+    document.body.appendChild(container);
 }
-document.body.appendChild(container);
+
+function mouseEnter() {
+    this.style.backgroundColor = 'skyblue';
+}
+
+function mouseLeave() {
+    this.style.backgroundColor = 'red';
+}
+createGrid();
